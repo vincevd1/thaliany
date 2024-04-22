504a0c451b9853b6969aec8fa487ef7d5cdbcc16
luuk schukkink <luuk.schukkink@gmail.com>
2024-04-22 15:27:03 +0200

    added names to users and made admin clearer

--- server/api/models.py
+++ server/api/models.py
@@ -3,17 +3,21 @@ from django.db import models
 class anyTimer(models.Model):
     owner_id = models.IntegerField()
     recipient_id = models.IntegerField()
+    owner_name = models.CharField(max_length=500)
+    recipient_name = models.CharField(max_length=500)
     amount = models.IntegerField()
     type = models.CharField(max_length=500)
     description = models.CharField(max_length=500, blank=True, null=True)
     def __str__(self):
-        return f"{self.owner_id} - {self.recipient_id}"
+        return f"{self.owner_name}'s anytimer on {self.recipient_name} for {self.amount} {self.type}"
     
 class anyTimerRequest(models.Model):
     requester_id = models.IntegerField()
     recipient_id = models.IntegerField()
+    requester_name = models.CharField(max_length=500)
+    recipient_name = models.CharField(max_length=500)
     amount = models.IntegerField()
     type = models.CharField(max_length=500)
     description = models.CharField(max_length=500, blank=True, null=True)
     def __str__(self):
-        return f"{self.requester_id} - {self.recipient_id}"
+        return f"{self.requester_name}s request for an anytimer on {self.recipient_name} for {self.amount} {self.type}"
