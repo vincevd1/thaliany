4045ffa57ffb9fd4d29e8300b0d9e4684bb80f84
luuk schukkink <luuk.schukkink@gmail.com>
2024-04-22 13:36:32 +0200

    added anytimer to admin django

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import anyTimer
 
 # Register your models here.
+class anyTimerAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # if the object is being edited
+            return ['owner_id', 'recipient_id']
+        else:
+            return []
+
+admin.site.register(anyTimer, anyTimerAdmin)
--- server/api/models.py
+++ server/api/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class anyTimer(models.Model):
     id = models.BigAutoField(primary_key=True)
     owner_id = models.IntegerField()
-    receipient_id = models.IntegerField()
+    recipient_id = models.IntegerField()
     amount = models.IntegerField()
     type = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    def __str__(self):
+        return f"{self.owner_id} - {self.recipient_id}"
     
 
