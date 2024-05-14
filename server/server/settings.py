13858a13079e9658b7b06418ffc8268b50cf9296
Dirk Doesburg <dirk.doesburg@gmail.com>
2024-05-14 15:44:30 +0200

    Fix debug mode

--- server/server/settings.py
+++ server/server/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", "1"))
+DEBUG = os.environ.get("DEBUG", "1") == "1"
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
